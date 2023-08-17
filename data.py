fc8f37cd5c6b4ec3762691900ac0c9ddf1a75005
nicolas.rosa <7662141@usp.br>
2023-08-17 10:07:58 -0300

    [Mod] data.py updated

--- data.py
+++ data.py
@@ -1,8 +1,10 @@
-import torch
-from torchvision.datasets.mnist import MNIST
+# --- Libraries
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from torchvision.datasets.mnist import MNIST
+# ---
 
+# --- Training and validation datasets
 data_train = MNIST("./data/mnist",
                    download=True,
                    train=True,
@@ -12,14 +14,18 @@ data_val = MNIST("./data/mnist",
                  train=False,
                  download=True,
                  transform=transforms.Compose([transforms.ToTensor()]))
+# ---
 
-dataloader_train = DataLoader(
-    data_train, batch_size=1000, shuffle=True, num_workers=8)
+# --- Training and validation dataloaders
+dataloader_train = DataLoader(data_train, batch_size=1000, shuffle=True, num_workers=8)
 dataloader_val = DataLoader(data_val, batch_size=1000, num_workers=8)
 
 dataloaders = {
     "train": dataloader_train,
     "val": dataloader_val,
 }
+# ---
 
-digit_one, _ = data_val[5]
+# --- Sample data, Digit "1"
+digit_one, _ = data_val[5]  # digit_one: (torch.Tensor, [1, 28,28], float32), label: (int)
+# ---
